40d0f960fd83f32b2a413c88b0a85ab33a8899cc
mono <monoberutu58@gmail.com>
2024-11-15 21:41:29 +0700

    deploy project

--- helper/response_ai.go
+++ helper/response_ai.go
@@ -11,34 +11,26 @@ import (
 )
 
 func ResponseAI(ctx context.Context, question string) (string, error) {
-	client, err := genai.NewClient(ctx, option.WithAPIKey("AIzaSyDH_LO9LZulVt6yxB7j0pNel8N5U8Adp7o"))
+	client, err := genai.NewClient(ctx, option.WithAPIKey("AIzaSyD3z_9LRn3x2J0VTW176jLDE0ilmf_wCMQ"))
 	if err != nil {
-		log.Println("Error creating AI client:", err)
-		return "", err
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-flash")
+	resp, err := model.GenerateContent(ctx, genai.Text("Write a story about a magic backpack."))
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Inisialisasi model generatif
-	modelAI := client.GenerativeModel("gemini-1.5-flash")
-	modelAI.SetTemperature(0) // Atur suhu ke 0 untuk respons deterministik
-
-	// Panggil API untuk menghasilkan konten
-	resp, err := modelAI.GenerateContent(ctx, genai.Text(question))
-	if err != nil {
-		log.Println("Error generating content:", err)
-		return "", err
-	}
-
-	// Pastikan respons memiliki kandidat
 	if len(resp.Candidates) == 0 || len(resp.Candidates[0].Content.Parts) == 0 {
 		log.Println("No response from AI model")
 		return "", fmt.Errorf("no response from AI model")
 	}
 
-	// Ambil respons pertama dari kandidat
 	answer := resp.Candidates[0].Content.Parts[0]
 	answerString := fmt.Sprintf("%v", answer)
 
-	// Bersihkan simbol tambahan dari teks
 	answerString = strings.ReplaceAll(answerString, "*", "")
 	answerString = strings.ReplaceAll(answerString, "**", "")
 	answerString = strings.ReplaceAll(answerString, "\n\n", " -")

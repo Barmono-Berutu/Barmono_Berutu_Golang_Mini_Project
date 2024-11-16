663854b6a7c08da9fb1aeb608a4509eae48de3c9
mono <monoberutu58@gmail.com>
2024-11-16 18:35:55 +0700

    deploy api project mini

--- helper/response_ai.go
+++ helper/response_ai.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
@@ -13,46 +12,27 @@ import (
 )
 
 func ResponseAI(ctx context.Context, question string) (string, error) {
-	// Get the API key from environment variable
-	apiKey := os.Getenv("AI_API_KEY")
-	if apiKey == "" {
-		log.Fatal("API Key is missing")
-		return "", fmt.Errorf("API Key is missing")
-	}
-
-	httpClient := &http.Client{}
-
-	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey), option.WithHTTPClient(httpClient))
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("AI_API_KEY")))
 	if err != nil {
-		log.Printf("Error creating client: %v", err)
-		return "", err
-	}
-	defer client.Close()
-
-	// Call the model
-	model := client.GenerativeModel("gemini-1.5-flash")
-	log.Println("Sending request to generative AI model...")
-	resp, err := model.GenerateContent(ctx, genai.Text(question))
-	if err != nil {
-		log.Printf("Error generating content: %v", err)
+		log.Fatal(err)
 		return "", err
 	}
 
-	// Process the response
-	if len(resp.Candidates) == 0 || len(resp.Candidates[0].Content.Parts) == 0 {
-		log.Println("No response from AI model")
-		return "", fmt.Errorf("no response from AI model")
+	modelAI := client.GenerativeModel("gemini-pro")
+	modelAI.SetTemperature(0)
+
+	resp, err := modelAI.GenerateContent(ctx, genai.Text(question))
+	if err != nil {
+		log.Fatal(err)
+		return "", err
 	}
 
-	var answerString string
-	for _, part := range resp.Candidates[0].Content.Parts {
-		answerString += fmt.Sprintf("%v", part)
-	}
+	answer := resp.Candidates[0].Content.Parts[0]
+	answerString := fmt.Sprintf("%v", answer)
 
-	// Clean the response string
+	// Bersihkan simbol tambahan dari teks
 	answerString = strings.ReplaceAll(answerString, "*", "")
-	answerString = strings.ReplaceAll(answerString, "**", "")
+	answerString = strings.ReplaceAll(answerString, "", "")
 	answerString = strings.ReplaceAll(answerString, "\n\n", " -")
-
 	return answerString, nil
 }

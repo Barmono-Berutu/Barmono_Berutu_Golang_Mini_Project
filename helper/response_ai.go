7f2ebd9ae7b74ab33b446086bbcc37ffac226318
mono <monoberutu58@gmail.com>
2024-11-16 09:11:02 +0700

    deploy api

--- helper/response_ai.go
+++ helper/response_ai.go
@@ -13,19 +13,14 @@ import (
 )
 
 func ResponseAI(ctx context.Context, question string) (string, error) {
-	// Get the API key from environment variable
 	apiKey := os.Getenv("AI_API_KEY")
 	if apiKey == "" {
-		log.Println("API Key is missing") // Ganti log.Fatal dengan log.Println untuk debugging
+		log.Fatal("API Key is missing")
 		return "", fmt.Errorf("API Key is missing")
 	}
 
-	log.Println("API Key successfully retrieved")
-
-	// Create default HTTP client
 	httpClient := &http.Client{}
 
-	// Create a new GenAI client
 	client, err := genai.NewClient(ctx, option.WithAPIKey(apiKey), option.WithHTTPClient(httpClient))
 	if err != nil {
 		log.Printf("Error creating client: %v", err)
@@ -33,9 +28,6 @@ func ResponseAI(ctx context.Context, question string) (string, error) {
 	}
 	defer client.Close()
 
-	log.Println("Client created successfully, calling the model")
-
-	// Call the model
 	model := client.GenerativeModel("gemini-1.5-flash")
 	resp, err := model.GenerateContent(ctx, genai.Text(question))
 	if err != nil {
@@ -43,9 +35,6 @@ func ResponseAI(ctx context.Context, question string) (string, error) {
 		return "", err
 	}
 
-	log.Println("Model response received")
-
-	// Process the response
 	if len(resp.Candidates) == 0 || len(resp.Candidates[0].Content.Parts) == 0 {
 		log.Println("No response from AI model")
 		return "", fmt.Errorf("no response from AI model")

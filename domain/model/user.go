af121285443d1392d135017ca993dc52ac9bc078
mono <monoberutu58@gmail.com>
2024-11-17 10:40:05 +0700

    Menghapus semua isi branch default

--- domain/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// Model untuk User
-type User struct {
-	ID             int              `json:"id" gorm:"primaryKey"`
-	Email          string           `json:"email"`
-	Password       string           `json:"password"`
-	AirQualityData []AirQualityData `gorm:"constraint:OnDelete:CASCADE;"`
-}
